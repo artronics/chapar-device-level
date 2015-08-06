34048ac00478f579fbaedaabe2495258d369c0e5
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-08-06 08:27:59 +0200

    create a class for all paths inside app

---
+++ src/main/java/it/unibo/sdwn/config/PathConfig.java
@@ -0,0 +1,13 @@
+package it.unibo.sdwn.config;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class PathConfig
+{
+
+    public final static Path BASE = Paths.get("/");
+    public final static Path CONFIG = Paths.get(BASE.toString(), "config");
+    public final static Path APP_CONFIG_FILE = Paths.get(BASE.toString(), CONFIG.toString(), "App.config");
+
+}
