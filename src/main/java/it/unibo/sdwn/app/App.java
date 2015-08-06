567f527d4ff2750b896a1287f236d9e98e3e1ec0
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-08-06 11:25:20 +0200

    update

--- src/main/java/it/unibo/sdwn/app/App.java
+++ src/main/java/it/unibo/sdwn/app/App.java
@@ -1,9 +1,14 @@
 package it.unibo.sdwn.app;
 
+import it.unibo.sdwn.config.Configuration;
+
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
+        Configuration con = new Configuration();
+        System.out.println(con.getAppConfig().getProperty("comPort"));
+        con.getAppConfig().setProperty("kjcomPort", "jalal");
     }
 }
