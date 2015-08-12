3d42a29ac347edc62c7c5ac8a4c440be428ef769
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-08-13 01:35:41 +0200

    delete conf for inode

--- src/main/java/it/unibo/sdwn/app/config/DependencyInjection.java
+++ src/main/java/it/unibo/sdwn/app/config/DependencyInjection.java
@@ -1,17 +1,9 @@
 package it.unibo.sdwn.app.config;
 
-import it.unibo.sdwn.node.INode;
-import it.unibo.sdwn.node.Node;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @org.springframework.context.annotation.Configuration
-@ComponentScan(value = {"it.unibo.sdwn.node"})
+@ComponentScan()
 public class DependencyInjection
 {
-    @Bean
-    public INode getINode()
-    {
-        return new Node();
-    }
 }
