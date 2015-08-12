743273a5d127f56955e3c60c0b0c8e695a7fb799
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-08-13 01:36:30 +0200

    node now implements inode

--- src/main/java/it/unibo/sdwn/app/Main.java
+++ src/main/java/it/unibo/sdwn/app/Main.java
@@ -1,10 +1,14 @@
 package it.unibo.sdwn.app;
 
+import it.unibo.sdwn.node.Address;
+
 public class Main
 {
     public static void main(String[] args)
     {
         App app = App.getInstance(args);
         app.init();
+        Address address = new Address(10);
+
     }
 }
--- src/main/java/it/unibo/sdwn/node/Node.java
+++ src/main/java/it/unibo/sdwn/node/Node.java
@@ -1,5 +1,23 @@
 package it.unibo.sdwn.node;
 
-public class Node {
+public class Node implements INode
+{
+    private Address address;
 
+    public Node(Address nodeAddr)
+    {
+        this.address = nodeAddr;
+    }
+
+    @Override
+    public Address getAddress()
+    {
+        return this.address;
+    }
+
+    @Override
+    public void setAddress(Address address)
+    {
+        this.address = address;
+    }
 }
