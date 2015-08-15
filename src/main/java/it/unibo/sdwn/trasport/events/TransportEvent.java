1c6b46533aa38bb38dcdcc8c262176bd2088f2d5
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-08-15 21:27:39 +0200

    add two bare event classes for Transport package.

---
+++ src/main/java/it/unibo/sdwn/trasport/events/TransportEvent.java
@@ -0,0 +1,7 @@
+package it.unibo.sdwn.trasport.events;
+
+import it.unibo.sdwn.app.event.AppBaseEvent;
+
+public class TransportEvent extends AppBaseEvent
+{
+}
---
+++ src/main/java/it/unibo/sdwn/trasport/events/TransportIsReady.java
@@ -0,0 +1,5 @@
+package it.unibo.sdwn.trasport.events;
+
+public class TransportIsReady extends TransportEvent
+{
+}
