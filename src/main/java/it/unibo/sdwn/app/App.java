df833c366c976d38902cc673f8db7d4451771adf
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-08-19 22:06:10 +0200

    refactoring dependency resolving.

--- src/main/java/it/unibo/sdwn/app/App.java
+++ src/main/java/it/unibo/sdwn/app/App.java
@@ -1,35 +1,18 @@
 package it.unibo.sdwn.app;
 
-import com.google.common.eventbus.Subscribe;
 import it.unibo.sdwn.app.commandLine.CommandLineOptionsManager;
-import it.unibo.sdwn.app.config.Config;
 import it.unibo.sdwn.app.event.RegisterHandler;
-import it.unibo.sdwn.app.logger.Log;
 import it.unibo.sdwn.controller.Controller;
-import it.unibo.sdwn.trasport.events.TransportIsReady;
 
 
 public final class App
 {
-    private static App instance = null;
 
     //Main dependencies
     private Controller controller;
 
-    private App()
+    public App()
     {
-    }
-
-    public static App getInstance()
-    {
-        if (instance == null) {
-            App app = new App();
-            instance = app;
-
-            return instance;
-        }
-
-        return instance;
     }
 
     public final synchronized Controller getController()
@@ -44,18 +27,16 @@ public final class App
         this.controller = controllerImpl;
     }
 
-    public void init(String[] appArgs)
+    public void initCommandLine(String[] appArgs)
     {
         CommandLineOptionsManager cmd = new CommandLineOptionsManager(appArgs);
         cmd.start();
+    }
+
+    public void initEventBus()
+    {
         //register all event handlers
         RegisterHandler.registerAll();
-
-        loadDependencies();
-        initDependencies();
-
-        //Run gui if available
-        runGui();
     }
 
     private void loadDependencies()
@@ -63,30 +44,11 @@ public final class App
         DependencyBuilder dependencyBuilder = new DependencyBuilder();
         dependencyBuilder.start();
         this.controller = dependencyBuilder.getController();
-
     }
 
-    private void initDependencies()
+    public void initDependencies()
     {
-        Log.main().debug("Start initializing dependency tree...");
+        loadDependencies();
         this.controller.init();
-        Log.main().debug("All dependencies are initialized successfully.");
     }
-
-    private void runGui()
-    {
-        final boolean hasGui = Config.get().getBoolean("hasGui");
-        if (hasGui) {
-
-        }
-    }
-
-
-    @Subscribe
-    public void transportIsready(TransportIsReady e)
-    {
-        Log.main().debug("trans is ready handler");
-    }
-
-
 }
