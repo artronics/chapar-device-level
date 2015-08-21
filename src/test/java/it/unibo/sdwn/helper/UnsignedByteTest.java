1deedebcecfb86ea709efb59c8992ea2f62b31cb
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-08-21 05:38:16 +0200

    adapt this test to junit4 and add equality assertion

--- src/test/java/it/unibo/sdwn/helper/UnsignedByteTest.java
+++ src/test/java/it/unibo/sdwn/helper/UnsignedByteTest.java
@@ -1,25 +1,43 @@
 package it.unibo.sdwn.helper;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
-public class UnsignedByteTest extends TestCase
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class UnsignedByteTest
 {
-    public void test_it_should_construct_with_short(){
+    @Test
+    public void Test_equality()
+    {
+        UnsignedByte first = new UnsignedByte(4);
+        UnsignedByte second = new UnsignedByte(4);
+
+        assertTrue(first.equals(second));
+        assertEquals(first, second);
+    }
+
+    @Test
+    public void test_it_should_construct_with_short()
+    {
         short shortValue = 100;
         UnsignedByte unsignedByte = new UnsignedByte(shortValue);
         assertEquals(shortValue, unsignedByte.shortValue());
     }
-    public void test_it_should_construct_with_int(){
+
+    @Test
+    public void test_it_should_construct_with_int()
+    {
         int intValue = 100;
         UnsignedByte unsignedByte = new UnsignedByte(intValue);
         assertEquals(intValue, unsignedByte.shortValue());
     }
 
-    @Test(expected= IllegalArgumentException.class)
-    public void should_throw_exp_on_overflow(){
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_exp_on_overflow()
+    {
         int intValue = 300;
-            UnsignedByte unsignedByte = new UnsignedByte(intValue);
+        UnsignedByte unsignedByte = new UnsignedByte(intValue);
     }
 
 }
