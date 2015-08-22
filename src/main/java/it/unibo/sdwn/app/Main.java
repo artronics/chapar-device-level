fefa491198839f8e7bb1ec1917b5d4cca391567f
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-08-22 15:27:47 +0200

    add jgrapht to dependencies. It is a graph lib

--- src/main/java/it/unibo/sdwn/app/Main.java
+++ src/main/java/it/unibo/sdwn/app/Main.java
@@ -2,6 +2,7 @@ package it.unibo.sdwn.app;
 
 import it.unibo.sdwn.app.config.Config;
 import it.unibo.sdwn.app.event.Event;
+import it.unibo.sdwn.node.Node;
 import it.unibo.sdwn.trasport.events.TransportEvent;
 
 public class Main
@@ -11,7 +12,6 @@ public class Main
         App app = new App();
         app.initCommandLine(args);
         app.initEventBus();
-
 
         Event.mainBus().post(new TransportEvent());
         String key = Config.get().getString("KEY");
