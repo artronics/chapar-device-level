0ce8ad110b7b03f9a4b0482aae52a1ab9326226a
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-08-24 01:08:03 +0200

    addByte to queue

--- src/main/java/it/unibo/sdwn/trasport/InOutPacketQueue.java
+++ src/main/java/it/unibo/sdwn/trasport/InOutPacketQueue.java
@@ -1,5 +1,25 @@
 package it.unibo.sdwn.trasport;
 
+import it.unibo.sdwn.helper.UnsignedByte;
+
 public class InOutPacketQueue implements InOutQueue
 {
+    public InOutPacketQueue()
+    {
+    }
+
+    @Override
+    public void addInput(byte[] buff, int length)
+    {
+        try {
+            inQueue.put(UnsignedByte.toUnsignedByteArray(buff, length));
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (byte b : buff) {
+            String msg = " " + UnsignedByte.of(b);
+            System.out.print(msg);
+        }
+        System.out.println();
+    }
 }
--- src/main/java/it/unibo/sdwn/trasport/InOutQueue.java
+++ src/main/java/it/unibo/sdwn/trasport/InOutQueue.java
@@ -1,23 +1,17 @@
 package it.unibo.sdwn.trasport;
 
-import it.unibo.sdwn.Packet.SdwnPacket;
 import it.unibo.sdwn.app.config.Config;
+import it.unibo.sdwn.helper.UnsignedByte;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public interface InOutQueue
 {
-    BlockingQueue inQueue = new ArrayBlockingQueue<SdwnPacket>(Config.get().getInt("MAX_QUEUE_CAPACITY"));
-    BlockingQueue outQueue = new ArrayBlockingQueue<SdwnPacket>(Config.get().getInt("MAX_QUEUE_CAPACITY"));
+    public static final int MAX_QUEUE_CAPACITY = Config.get().getInt("MAX_QUEUE_CAPACITY");
+    BlockingQueue<UnsignedByte[]> inQueue = new ArrayBlockingQueue(MAX_QUEUE_CAPACITY);
+    BlockingQueue outQueue = new ArrayBlockingQueue(MAX_QUEUE_CAPACITY);
 
-    static BlockingQueue getOutQueue()
-    {
-        return outQueue;
-    }
-
-    static BlockingQueue getInQueue()
-    {
-        return inQueue;
-    }
+    void addInput(byte[] buff, int length);
 }
+
