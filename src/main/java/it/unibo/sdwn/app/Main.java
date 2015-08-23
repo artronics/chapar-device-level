d316107357cbd1e62aba5484da50040072c60072
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-08-24 01:08:52 +0200

    resove dependency in main add correct app ref in xml

--- src/main/java/it/unibo/sdwn/app/Main.java
+++ src/main/java/it/unibo/sdwn/app/Main.java
@@ -1,22 +1,33 @@
 package it.unibo.sdwn.app;
 
-import com.google.common.eventbus.EventBus;
-import it.unibo.sdwn.app.config.Config;
-import it.unibo.sdwn.app.event.Event;
-import it.unibo.sdwn.trasport.events.TransportEvent;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        App app = new App();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("DependencyInjection.xml");
+        App app = context.getBean(App.class);
         app.initCommandLine(args);
         app.initEventBus();
+        app.start();
+        long startTime = System.currentTimeMillis();
 
-
-        EventBus eventBus = Event.mainBus();
-        eventBus.post(new TransportEvent());
-        String key = Config.get().getString("KEY");
-        System.out.println(key);
+        byte total = 0;
+        int initialCapacity = 1000000;
+//        ArrayList<UnsignedByte> b = new ArrayList<>(initialCapacity);
+//        UnsignedByte[] b= new UnsignedByte[initialCapacity];
+//        for (int i = 0; i < initialCapacity; i++) {
+////            b.add(UnsignedByte.of(0));
+//            b[i]=UnsignedByte.of(0);
+//        }
+//        byte[] bb = new byte[initialCapacity] ;
+//        for (int i = 0; i < initialCapacity; i++) {
+//            bb[i]=0;
+//        }
+//
+//        long stopTime = System.currentTimeMillis();
+//        long elapsedTime = stopTime - startTime;
+//        System.out.println(elapsedTime);
     }
 }
