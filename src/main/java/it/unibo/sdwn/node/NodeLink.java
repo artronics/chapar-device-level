589bb2afc4d42f55e34b0ee6bd5a5388b443b057
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-08-26 16:07:09 +0200

    add comment

--- src/main/java/it/unibo/sdwn/node/NodeLink.java
+++ src/main/java/it/unibo/sdwn/node/NodeLink.java
@@ -2,6 +2,10 @@ package it.unibo.sdwn.node;
 
 public final class NodeLink implements Link
 {
+    //Since the link is reciprocal there
+    //is no difference between sourceNode and
+    //destinationNode there are simply two ends
+    //of a relationship
     private final Node sourceNode;
     private final Node destinationNode;
     private Quality quality;
