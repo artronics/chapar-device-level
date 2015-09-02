5ffca6ea37329c8dc843de3e6a225e5bd30b4afb
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-09-02 21:49:28 +0200

    remove unused imports

--- src/test/java/artronics/chapar/node/BaseNodeTest.java
+++ src/test/java/artronics/chapar/node/BaseNodeTest.java
@@ -1,11 +1,10 @@
 package artronics.chapar.node;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 public class BaseNodeTest
