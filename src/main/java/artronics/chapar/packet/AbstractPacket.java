c58a17d8ba91d00d860b6a561de78e9c1d612a1f
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-09-03 17:49:57 +0200

    place UnssuportedException to all methods. later we should implement skeletal methods.

--- src/main/java/artronics/chapar/packet/AbstractPacket.java
+++ src/main/java/artronics/chapar/packet/AbstractPacket.java
@@ -3,6 +3,7 @@ package artronics.chapar.packet;
 import artronics.chapar.core.analyser.Analysable;
 import artronics.chapar.core.logger.Log;
 
+import javax.naming.OperationNotSupportedException;
 import java.util.List;
 
 public abstract class AbstractPacket implements Packet
@@ -12,7 +13,51 @@ public abstract class AbstractPacket implements Packet
     {
     }
 
+    @Override
+    public List getAll()
+    {
+        throw new UnsupportedOperationException();
+    }
 
+    @Override
+    public List getHeader()
+    {
+        throw new UnsupportedOperationException();
+    }
 
+    @Override
+    public int getHeaderSize()
+    {
+        throw new UnsupportedOperationException();
+    }
 
+    @Override
+    public int getDataSize()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List getData()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getType()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public long getSerialNumber()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        throw new UnsupportedOperationException();
+    }
 }
