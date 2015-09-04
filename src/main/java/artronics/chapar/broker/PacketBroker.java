114b4310249bb30b6c0db6ec1296a5ab95418272
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-09-04 14:00:35 +0200

    remove try catch block. that was there for debuging event bus exp

--- src/main/java/artronics/chapar/broker/PacketBroker.java
+++ src/main/java/artronics/chapar/broker/PacketBroker.java
@@ -4,7 +4,6 @@ import artronics.chapar.core.events.DataInEvent;
 import artronics.chapar.core.events.Event;
 import artronics.chapar.core.events.PacketInEvent;
 import artronics.chapar.packet.Packet;
-import artronics.chapar.packet.PacketFactory;
 import com.google.common.eventbus.Subscribe;
 
 import java.util.List;
@@ -28,19 +27,15 @@ public class PacketBroker
     @Subscribe
     public void dataInEventHandler(DataInEvent event)
     {
-        try {
-            System.out.println("kir tush");
-            while (!inputMsg.isEmpty()) {
-                List<Integer> message = inputMsg.take();
-                final List<Packet> packets = convertor.generatePackets(message);
-                for (Packet packet : packets) {
-                    packetsIn.put(packet);
-                    Event.mainBus().post(new PacketInEvent());
-                }
+        System.out.println("kir tush");
+        while (!inputMsg.isEmpty()) {
+            List<Integer> message = inputMsg.take();
+            final List<Packet> packets = convertor.generatePackets(message);
+            for (Packet packet : packets) {
+                System.out.println("fooo");
+                packetsIn.put(packet);
+                Event.mainBus().post(new PacketInEvent());
             }
-        }catch (Exception e) {
-            System.out.println("madareto");
-            e.printStackTrace();
         }
     }
 }
