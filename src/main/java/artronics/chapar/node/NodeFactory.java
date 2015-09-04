3f0320b605aac3d6f08f4a637dd924646ebdd505
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-09-04 17:02:37 +0200

    delete BaseNodeFactory and its interface

--- src/main/java/artronics/chapar/node/NodeFactory.java
+++
@@ -1,8 +0,0 @@
-package artronics.chapar.node;
-
-public interface NodeFactory
-{
-    Node create(int address, Node.NodeType type);
-    Node createNode(int address);
-    Node createSink(int address);
-}
